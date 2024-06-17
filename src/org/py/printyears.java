e6e1af3b982a0649c0b96ea30719308dcb6fbf9b
paripalani <itachiravan@gmail.com>
2024-06-17 17:19:28 +0530

    years from 1998 to 2020

---
+++ src/org/py/printyears.java
@@ -0,0 +1,12 @@
+package org.py;
+
+public class printyears {
+	
+	    public static void main(String[] args) {
+	        for (int year = 1998; year <= 2020; year++) {
+	            System.out.println(year);
+	        }
+	    }
+	}
+
+
